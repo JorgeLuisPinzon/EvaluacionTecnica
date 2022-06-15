1cdf1c5d5c7fb24f952772e6f042721e330153ba
pinzon_08@hotmail.com <Jorge Luis Pinzon>
2022-06-15 01:29:39 -0500

    Commit ajuste codigo

--- src/main/java/com/evaluacion/Utils/AplicationConstants.java
+++ src/main/java/com/evaluacion/Utils/AplicationConstants.java
@@ -1,7 +1,10 @@
 package com.evaluacion.Utils;
 
+/**
+ * @author jorge
+ *
+ */
 public class AplicationConstants {
-
 	public  static final String C_DATO_NO_ENCONTRADO ="No se encontrado registro";
 	public  static final String C_ERROR_UPDATE ="Error al Actualizar Usuario con ID ";
 	public  static final String C_ERROR_DELETE ="Error al Eliminar Usuario con ID: ";
@@ -12,5 +15,4 @@ public class AplicationConstants {
 	public  static final String C_INGRESO_FORMAT_MAYOR_0	= "El Formato para ingreso debe ser mayor que 0";
 	public  static final String C_NUMBER_CTA_FORMAT_10_DIG = "El Numero de cuenta debe estar conformado por 10 Digitos"; 
 	public  static final String C_COD_POSTAL_FORMAT_6_DIG= "El Codigo Postal debe estar conformado por 6 Digitos"; 
- 
 }
